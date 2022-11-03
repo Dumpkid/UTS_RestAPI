a895494dac7c7d2e13ebd6e73f032a2e175694dc
Dumpkid <fjrkrnwn58@gmail.com>
2022-11-04 01:56:04 +0700

    add http status code

--- method/method.php
+++ method/method.php
@@ -3,6 +3,11 @@
     class Buku{
 
         function getBuku(){
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json; charset=UTF-8");
+            header("Access-Control-Allow-Methods: GET");
+            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
             global $conn;
             $query = $conn->query("SELECT * FROM databuku");
             while($row = mysqli_fetch_object($query)){
@@ -13,15 +18,20 @@
                 'message' => 'Get Data Success',
                 'data' => $data
             );
-            header('Content-Type: application/json');
+            http_response_code(200);
             echo json_encode($response);
         }
         
         function getBukuId($id=0){
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json; charset=UTF-8");
+            header("Access-Control-Allow-Methods: GET");
+            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
             global $conn;
             $query = "SELECT * FROM databuku";
             if($id !=0){
-                $query.=" WHERE id".$id."LIMIT 1";
+                $query .=" WHERE id_buku = ".$id." LIMIT 1";
             }
             $data = array();
             $result = $conn->query($query);
@@ -34,14 +44,14 @@
                     'message' => 'Get Data Success',
                     'data' => $data
                 );
+                http_response_code(200);
             }else{
                 $response = array(
                     'status' => false,
                     'message' => 'Tidak tidak ditemukan'
                 );
+                http_response_code(404);
             }
-        
-            header('Content-Type: application/json');
             echo json_encode($response);
         }
 
@@ -56,7 +66,7 @@
                 'edisi' => '',
                 'jumlah_buku' => '',
                 'halaman' => '',
-                'tanggal_masuk' => '',
+                // 'tanggal_masuk' => '',
                 'jenis_buku' => '',
                 'isbn' => '',
                 'sumber' => '',
@@ -70,11 +80,12 @@
                 judul ='$_POST[judul]',
                 pengarang = '$_POST[pengarang]',
                 tempat_terbit = '$_POST[tempat_terbit]',
+                penerbit = '$_POST[penerbit]',
                 tahun_terbit = '$_POST[tahun_terbit]',
                 edisi = '$_POST[edisi]',
                 jumlah_buku = '$_POST[jumlah_buku]',
                 halaman = '$_POST[halaman]',
-                tanggal_masuk = '$_POST[tanggal_masuk]',
+                -- tanggal_masuk = '$_POST[tanggal_masuk]',
                 jenis_buku = '$_POST[jenis_buku]',
                 isbn = '$_POST[isbn]',
                 sumber = '$_POST[sumber]',
@@ -87,6 +98,7 @@
                         'status' => true,
                         'message' => 'Add Data Success'
                     );
+                    http_response_code(201);
                 }else{
                     $response = array(
                         'status' => false,
@@ -98,12 +110,18 @@
                     'status' => false,
                     'message' => 'Parametes Do Not Match'
                 );
+                http_response_code(400);
             }
             header('Content-Type: application/json');
             echo json_encode($response);
         }
 
         function updateBuku($id){
+            header("Access-Control-Allow-Origin: *");
+            header("Content-Type: application/json; charset=UTF-8");
+            header("Access-Control-Allow-Methods: POST");
+            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
             global $conn;
             $arrcheckpost = array(
                 'judul' => '',
@@ -147,19 +165,23 @@
                         'status' => true,
                         'message' => 'Update Data Success'
                     );
+                    http_response_code(201);
                 }else{
                     $response = array(
                         'status' => false,
                         'message' => 'Update Data Failed'
                     );
+                    http_response_code(400);
+                    
                 }
             }else{
                 $response = array(
                     'status' => false,
                     'message' => 'Parameter Do Not Match'
                 );
+                http_response_code(400);
+                
             }
-            header('Content-Type: application/json');
             echo json_encode($response);
         }
 
@@ -172,11 +194,13 @@
                     'status' => true,
                     'message' => 'Delete Data Success'
                 );
+                http_response_code(200);
             }else{
                 $response = array(
                     'status' => false,
                     'message' => 'Delete Data Failed'
-            );
+                );
+                http_response_code(404);
             }
             header('Content-Type: application/json');
             echo json_encode($response);
